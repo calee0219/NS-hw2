045642b5d9f2cf0aacd51e2e3d58b593f2985312
eggegg31415 <eggegg31415.cs06@g2.nctu.edu.tw>
2020-06-13 22:38:27 +0800

    Add decision tree traning model
    Add decision tree training model to main.py

--- Group_32/Project_code/data.py
+++ Group_32/Project_code/data.py
@@ -8,8 +8,12 @@ Date: 2020/06/13
 
 import sys
 import os
+import numpy as np
 
 import xmltodict
+import sklearn
+import pandas as pd
+event_name = "Image"
 
 
 def usage():
@@ -23,25 +27,12 @@ def load_xml(path):
         xml_data = xmltodict.parse(reader.read())
     return xml_data
 
-
-def main():
-    """main function"""
-    if len(sys.argv) != 2:
-        usage()
-        return
-    folder_path = str(sys.argv[1])
-    print("Training data folder: ", folder_path)
-    subfolder = os.listdir(folder_path)
-    sysmon = []
-    for person in subfolder:
-        # Sysmon
-        sysmon_dict = load_xml(os.path.join(folder_path, person, "Sysmon.xml"))
-        sysmon.append(sysmon_dict)
-
-    # Code added by yian
-    feature_table = [[]]
-    event_name = 'OriginalFileName'
-    for num in range(1, len(sysmon)):
+def collect_sysmon(sysmon):
+    """Collect sysmon data"""
+    feature_table = []
+    feature_name = []
+    tot_feature = 0
+    for num in range(len(sysmon)):
         sysmon_data = sysmon[num]
         cnt = {}
         for event in sysmon_data['Events']['Event']:
@@ -53,27 +44,47 @@ def main():
                     else:
                         cnt[ori] = 1
 
-        feature_table.append([0 for i in range(len(feature_table[num-1]))])
+        feature_table.append([0 for i in range(tot_feature)])
         for key, value in cnt.items():
             try:
-                feature_num = feature_table[0].index(key)
+                feature_num = feature_name.index(key)
                 feature_table[num][feature_num] = value
             except:
-                feature_table[0].append(key)
+                feature_name.append(key)
                 feature_table[num].append(value)
-
-        tot_feature = len(feature_table[0])
-        for row in feature_table:
-            row_len = len(row)
-            for i in range(tot_feature-row_len):
-                row.append(0)
+                tot_feature += 1
+    for row in feature_table:
+        row_len = len(row)
+        for i in range(tot_feature-row_len):
+            row.append(0)
 
     # Write sysmon data into sysmon_data.py
-    with open("./sysmon_data.py", 'w') as writer:
-        writer.write("sysmon_matrix = ")
+    with open("./training_data.py", 'w') as writer:
+        writer.write("event_name = ")
+        writer.write('"'+event_name+'"')
+        writer.write("\nsysmon_feature_name = ")
+        writer.write(str(feature_name))
+        writer.write("\nsysmon_matrix = ")
         writer.write(str(feature_table))
         writer.close()
 
+
+def main():
+    """main function"""
+    if len(sys.argv) != 2:
+        usage()
+        return
+    folder_path = str(sys.argv[1])
+    print("Training data folder: ", folder_path)
+    subfolder = sorted(os.listdir(folder_path))
+    sysmon = []
+    for person in subfolder:
+        print(person)
+        # Sysmon
+        sysmon_dict = load_xml(os.path.join(folder_path, person, "Sysmon.xml"))
+        sysmon.append(sysmon_dict)
+
+    collect_sysmon(sysmon)
 
 if __name__ == "__main__":
     main()
--- Group_32/Project_code/main.py
+++ Group_32/Project_code/main.py
@@ -6,12 +6,10 @@ import sys
 import os
 import json
 
-#import xml.etree.ElementTree as ET
 import xmltodict
-import collections
-orderedDict = collections.OrderedDict()
-from collections import OrderedDict
-#import numpy as np
+import numpy as np
+import pandas as pd
+from sklearn import tree
 
 from sysmon_data import *
 
@@ -37,9 +35,12 @@ def load_xml(path):
     return xml_data
 
 
-def load_training_data():
-    """Load training data"""
-    print(sysmon_matrix)
+def decision_tree(x):
+    """Run Decision Tree"""
+    y = [1, 2, 3, 4, 5, 6]
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(x, y)
+    return clf
 
 
 def main():
@@ -50,18 +51,32 @@ def main():
     folder_path = str(sys.argv[1])
     print("Testing folder: ", folder_path)
     subfolder = os.listdir(folder_path)
+
+    # Load Training data
+    sysmon_clf = decision_tree(sysmon_matrix)
+
     for testcase in subfolder:
         print("{index}: ".format(index=testcase), end='')
-        wireshark_data = load_json(os.path.join(folder_path, testcase, "Wireshark.json"))
         sysmon_data = load_xml(os.path.join(folder_path, testcase, "Sysmon.xml"))
-        security_data = load_xml(os.path.join(folder_path, testcase, "Security.xml"))
-        #print(wireshark_data)
-        #print(sysmon_data)
-        #print(security_data)
-        print(end="\n")
-    # Load Training data
-    df = load_training_data()
+        cnt = {}
+        for event in sysmon_data['Events']['Event']:
+            for data in event['EventData']['Data']:
+                if data['@Name'] == event_name:
+                    feature = data['#text']
+                    if feature in cnt.keys():
+                        cnt[feature] += 1
+                    else:
+                        cnt[feature] = 1
 
+        predict = []
+        for feature in sysmon_feature_name:
+            try:
+                predict.append(cnt[feature])
+            except:
+                predict.append(0)
+        print(predict)
+        ans = sysmon_clf.predict([predict])
+        print(ans)
 
 if __name__ == "__main__":
     main()
